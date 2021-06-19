7c5bfdf7b9c6d0b34049b57a83a0733a0dec9590
RahimovMBR <rahimovmbr@mail.ru>
2021-06-19 22:09:34 +0500

    faylar qoshildi

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import*
 
 urlpatterns = [
-    path('',add),
-    path('',remove),
+    path('add/',add),
+    path('remove/',remove),
+    path('',index)
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
-# Create your views here.
 def add(request):
-    return
-
-
+    file_name='db.json'
+    capital={"davlatlar":[]}
+    davlat=request.GET.get('dav')
+    poytaxt=request.GET.get('poy')
+    new={davlat:poytaxt}
+    capital['davlatlar'].append(new)
+    with open(file_name,'a') as file:
+        capital=capital['davlatlar']
+        json.dump(capital,file)
+    return HttpResponse(f'{capital}')
 def remove(request):
-    return    
+    return HttpResponse('3')    
+
+
+def index(request):
+    
+    return HttpResponse('4')        
